e9dc2c313b1eb44462f74a04c5eb3aeed47e61b7
Milan Mundhra <78230444+milan0027@users.noreply.github.com>
2021-10-09 05:54:25 +0530

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -9,10 +9,14 @@ using namespace std;
 #define vpii vector<pair<int,int>>
 #define loop(n) for(int i=0;i<n;i++)
 #define rep(i,s,e) for(int i=s;i<e;i++)
+#define take_in(a,n) int n; cin>>n; vector<int> a(n); for(int i=0;i<n;i++) cin>>a[i];
 #define test int t; cin>>t; while(t--)
 #define sortf(a) sort(a.begin(),a.end())
 #define sortr(a) sort(a.rbegin(),a.rend())
 #define all(a) a.begin(),a.end()
+#define get_sum(a) accumulate(a.begin(),a.end(),0LL)
+#define get_max(a) *max_element(a.begin(),a.end())
+#define get_min(a) *min_element(a.begin(),a.end())
 #define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define ansyes cout<<"YES\n"
 #define ansno cout<<"NO\n"
@@ -25,23 +29,7 @@ int32_t main()
 {
 
    fastio;
-   int isprime[101]={0};
-   isprime[1]=1;
-   for(int i=2;i*i<=100;i++)
-   {
-      if(isprime[i]==0)
-      {
-         for(int j=i*i;j<=100;j+=i)
-         {
-            isprime[j]=1;
-         }
-      }
-   }
-   for(int i=1;i<=100;i++)
-   {
-      if(isprime[i]==0)
-         cout<<i<<" ";
-   }
+   //code goes here
    
 
 }
